e9674a2ebdcf6a143a64018f69a87b0ceef0a49b
agaverdi <adil.agaverdiyev@mail.ru>
2020-05-23 14:36:14 +0400

    front

---
+++ main.js
@@ -0,0 +1,15 @@
+function navFunction(){
+    var burger_menu=document.querySelector('.nav-menu-burger');
+    if(burger_menu.style.display==='block'){
+        burger_menu.style.display='none';
+    }
+    else{
+        
+        burger_menu.style.display='block';
+    }
+}
+
+
+
+
+
